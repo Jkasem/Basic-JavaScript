731ae443d95855d737b6019be0ed774aeeb8bbde
Justin Kaseman <justinkaseman@live.com>
2018-02-19 10:49:13 -0800

    Pass project-3

--- src/project-3.js
+++ src/project-3.js
@@ -5,12 +5,12 @@ const makeCat = (name, age) => {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
-  this.name = name;
-  this.age = age;
-  this.meow = function () {
-    return 'Meow!';
+  const newCat = {
+    name,
+    age,
+    meow: x => ('Meow!')
   };
-  return this;
+  return newCat;
 };
 
 const addProperty = (object, property) => {
@@ -25,7 +25,7 @@ const invokeMethod = (object, method) => {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
-  object.method();
+  object[method]();
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
@@ -44,35 +44,48 @@ const deleteProperty = (object, property) => {
 const newUser = (name, email, password) => {
   // create a new object with properties matching the arguments passed in.
   // return the new object
-
+  const createUser = {
+    name,
+    email,
+    password
+  };
+  return createUser;
 };
 
 const hasEmail = (user) => {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+  return !!user.email;
 };
 
 const hasProperty = (object, property) => {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  return Object.prototype.hasOwnProperty.call(object, property);
 };
 
 const verifyPassword = (user, password) => {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  if (user.password === password) return true;
+  return false;
 };
 
 const updatePassword = (user, newPassword) => {
   // replace the existing password on the user object with the value of newPassword
   // return the object
+  user.password = newPassword;
+  return user;
 };
 
 const addFriend = (user, newFriend) => {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
   // return the user object
+  user.friends.push(newFriend);
+  return user;
 };
 
 const setUsersToPremium = (users) => {
@@ -80,6 +93,8 @@ const setUsersToPremium = (users) => {
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
   // return the users array
+  users.map(x => x.isPremium = true);
+  return users;
 };
 
 const sumUserPostLikes = (user) => {
@@ -88,6 +103,11 @@ const sumUserPostLikes = (user) => {
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
   // return the sum
+  let totalLikes = 0;
+  for (let i = 0; i < user.posts.length; i++) {
+    totalLikes += user.posts[i].likes;
+  }
+  return totalLikes;
 };
 
 const addCalculateDiscountPriceMethod = (storeItem) => {
@@ -100,6 +120,10 @@ const addCalculateDiscountPriceMethod = (storeItem) => {
   // discountPrice = 20 - (20 * .2)
   // Make sure you return storeItem after adding the method to it
   // hint: arrow functions don't bind a this
+  storeItem.calculateDiscountPrice = function () {
+    return storeItem.price - (storeItem.price * storeItem.discountPercentage);
+  };
+  return storeItem;
 };
 
 // Do not modify code below this line.
